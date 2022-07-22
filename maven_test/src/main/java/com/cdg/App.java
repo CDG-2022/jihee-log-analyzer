4249b9206799fe94cedd3aeedd51d1a741e94bff
GHeeJeon <jhjeon_99@naver.com>
2022-07-22 20:19:54 +0900

    main-write file

--- maven_test/src/main/java/com/cdg/App.java
+++ maven_test/src/main/java/com/cdg/App.java
@@ -1,6 +1,7 @@
 package com.cdg;
 
 import java.io.File;  // Import the File class
+import java.io.FileWriter;   // Import the FileWriter class
 import java.io.IOException;  // Import the IOException class to handle error
 
 /**
@@ -12,12 +13,10 @@ public class App
     public static void main( String[] args )
     {
         try {
-            File myObj = new File("filename.txt");
-            if (myObj.createNewFile()) {
-                System.out.println("File created: " + myObj.getName());
-            } else {
-                System.out.println("File already exists.");
-            }
+            FileWriter myWriter = new FileWriter("filename.txt");
+            myWriter.write("Files in Java might be tricky, but it is fun enough!");
+            myWriter.close();
+            System.out.println("Successfully wrote to the file.");
         } catch (IOException e) {
             System.out.println("An error occurred.");
             e.printStackTrace();
