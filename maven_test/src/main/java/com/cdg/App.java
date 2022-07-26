f793d27515a2833fb6caca4f333965f696a9b41a
GHeeJeon <jhjeon_99@naver.com>
2022-07-26 21:45:21 +0900

    main-make log template
    just template!

--- maven_test/src/main/java/com/cdg/App.java
+++ maven_test/src/main/java/com/cdg/App.java
@@ -10,10 +10,8 @@ import java.util.Scanner; // Import the Scanner class to read text files
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
+public class App {
+    public static void main(String[] args) {
         try {
             File myObj = new File("input.log");
             Scanner myReader = new Scanner(myObj);
@@ -23,6 +21,39 @@ public class App
             }
             myReader.close();
         } catch (FileNotFoundException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+
+        try {
+            File myObj = new File("output.log");
+            if (myObj.createNewFile()) {
+                System.out.println("File created: " + myObj.getName());
+            } else {
+                System.out.println("File already exists.");
+            }
+            FileWriter myWriter = new FileWriter("output.log");
+
+            myWriter.write("최다호출 APIKEY\n\n");
+            myWriter.write("상태코드 별 횟수\n\n" +
+                    "10 : " + "\n" +
+                    "200 : " + "\n" +
+                    "404 : " + "\n\n");
+            myWriter.write("상위 3개의 API ServiceID와 각각의 요청 수\n\n" +
+                    "blog : " + "\n" +
+                    "vdip : " + "\n" +
+                    "image : " + "\n\n");
+            myWriter.write("피크 시간대\n\n");
+            myWriter.write("웹 브라우저 별 사용비율\n\n" +
+                    "IE : " + "\n" +
+                    "Firefox : " + "\n" +
+                    "Safari : " + "\n" +
+                    "Chrome : " + "\n" +
+                    "Opera : " + "\n\n");
+
+            myWriter.close();
+            System.out.println("Successfully wrote to the file.");
+        } catch (IOException e) {
             System.out.println("An error occurred.");
             e.printStackTrace();
         }
