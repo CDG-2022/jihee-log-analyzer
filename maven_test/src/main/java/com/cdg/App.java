a7e1745e94eb55e3d9905ee06212ff953f791361
GHeeJeon <jhjeon_99@naver.com>
2022-07-27 02:07:29 +0900

    pr-review-PR용 커밋

--- maven_test/src/main/java/com/cdg/App.java
+++ maven_test/src/main/java/com/cdg/App.java
@@ -12,11 +12,12 @@ import java.util.Scanner; // Import the Scanner class to read text files
  */
 public class App {
     public static void main(String[] args) {
+        String data;
         try {
             File myObj = new File("input.log");
             Scanner myReader = new Scanner(myObj);
             while (myReader.hasNextLine()) {
-                String data = myReader.nextLine();
+                data = myReader.nextLine();
                 System.out.println(data);
             }
             myReader.close();
