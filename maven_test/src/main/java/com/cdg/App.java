6e672570ce28a50d67782bf613b3d22c14165627
GHeeJeon <jhjeon_99@naver.com>
2022-08-02 22:46:37 +0900

    pr-review-use hashmap

--- maven_test/src/main/java/com/cdg/App.java
+++ maven_test/src/main/java/com/cdg/App.java
@@ -6,17 +6,24 @@ import java.io.File;  // Import the File class
 import java.io.FileWriter;   // Import the FileWriter class
 import java.io.IOException;  // Import the IOException class to handle error
 import java.io.FileNotFoundException;  // Import this class to handle errors
-import java.util.Scanner; // Import the Scanner class to read text files
+import java.util.*;
 
 /**
  * Hello world!
  */
 public class App {
+    private static HashMap<String, Integer> apiKey = new HashMap<>();
+    private static HashMap<String, Integer> apiService = new HashMap<>();
+    private static HashMap<String, Integer> peakTime = new HashMap<>();
+    private static HashMap<String, Integer> webBrowser = new HashMap<>();
+
     public static void main(String[] args) {
         String data = "";
+        int logCount = 0;
         int serverCodeCount10 = 0;
         int serverCodeCount200 = 0;
         int serverCodeCount404 = 0;
+
         try {
             File myObj = new File("input.log");
             Scanner myReader = new Scanner(myObj);
@@ -27,6 +34,13 @@ public class App {
                 serverCodeCount200 = serverCodeCount200 + StringUtils.countMatches(data, "[200]");
                 serverCodeCount404 = serverCodeCount404 + StringUtils.countMatches(data, "[404]");
 
+                String[] arrayData = StringUtils.substringsBetween(data, "[", "]");
+                apiKey.put(StringUtils.substringBetween(arrayData[1], "apikey=", "&"), apiKey.getOrDefault(StringUtils.substringBetween(arrayData[1], "apikey=", "&"), 0) + 1);
+                apiService.put(StringUtils.substringBetween(arrayData[1], "search/", "?"), apiService.getOrDefault(StringUtils.substringBetween(arrayData[1], "search/", "?"), 0) + 1);
+                peakTime.put(arrayData[3].substring(0, arrayData[3].length()-3), peakTime.getOrDefault(arrayData[3].substring(0, arrayData[3].length()-3), 0) + 1);
+                webBrowser.put(arrayData[2], webBrowser.getOrDefault(arrayData[2], 0) + 1);
+
+                logCount++;
                 System.out.println(data);
             }
             myReader.close();
@@ -45,23 +59,24 @@ public class App {
 
             FileWriter myWriter = new FileWriter("output.log");
 
-            myWriter.write("최다호출 APIKEY\n\n");
+            myWriter.write("로그 수: " + logCount + "\n\n");
+
+            myWriter.write("최다호출 APIKEY"+ "\n\n" + apiKey + "\n\n");
             myWriter.write("상태코드 별 횟수\n\n" +
                     "10 : " + serverCodeCount10 + "\n" +
                     "200 : " + serverCodeCount200 + "\n" +
                     "404 : " + serverCodeCount404 + "\n\n");
-            myWriter.write("상위 3개의 API ServiceID와 각각의 요청 수\n\n" +
+            myWriter.write("상위 3개의 API ServiceID와 각각의 요청 수\n\n" + apiService + "\n" +
                     "blog : " + "\n" +
                     "vdip : " + "\n" +
                     "image : " + "\n\n");
-            myWriter.write("피크 시간대\n\n");
-            myWriter.write("웹 브라우저 별 사용비율\n\n" +
+            myWriter.write("피크 시간대\n\n" + peakTime);
+            myWriter.write("웹 브라우저 별 사용비율\n\n" + webBrowser + "\n" +
                     "IE : " + "\n" +
                     "Firefox : " + "\n" +
                     "Safari : " + "\n" +
                     "Chrome : " + "\n" +
                     "Opera : " + "\n\n");
-
 
             myWriter.close();
             System.out.println("변경사항을 저장했어요!");
