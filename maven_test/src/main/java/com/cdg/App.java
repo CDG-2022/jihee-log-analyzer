6ca624c07b4dda13924b736cf7a0046f5eb9d837
GHeeJeon <jhjeon_99@naver.com>
2022-08-07 17:50:53 +0900

    pr-review-solve 최다호출 APIKEY, API ServiceID
    리팩토링 절실... 누가 이렇게 코드를 짜ㅠㅠㅠㅠ

--- maven_test/src/main/java/com/cdg/App.java
+++ maven_test/src/main/java/com/cdg/App.java
@@ -50,13 +50,48 @@ public class App {
             e.printStackTrace();
         }
 
-        Map<String, Integer> orderedMap = PEAK_TIME.entrySet()
+        Map<String, Integer> orderedMapApiKey = API_KEY.entrySet()
                 .stream()
                 .sorted(Map.Entry.<String, Integer>comparingByValue().reversed())
                 .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue, (oldValue, newValue) -> newValue, LinkedHashMap::new));
 
-        final Iterator<Map.Entry<String, Integer>> entryIterator = orderedMap.entrySet().iterator();
-        System.out.println(entryIterator.next().getKey());
+        Map<String, Integer> orderedMapAPIService = API_SERVICE.entrySet()
+                .stream()
+                .sorted(Map.Entry.<String, Integer>comparingByValue().reversed())
+                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue, (oldValue, newValue) -> newValue, LinkedHashMap::new));
+
+        Map<String, Integer> orderedMapPeakTime = PEAK_TIME.entrySet()
+                .stream()
+                .sorted(Map.Entry.<String, Integer>comparingByValue().reversed())
+                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue, (oldValue, newValue) -> newValue, LinkedHashMap::new));
+
+        Map<String, Integer> orderedMapWebBrowser = WEB_BROWSER.entrySet()
+                .stream()
+                .sorted(Map.Entry.<String, Integer>comparingByValue().reversed())
+                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue, (oldValue, newValue) -> newValue, LinkedHashMap::new));
+
+        final Iterator<Map.Entry<String, Integer>> entryIteratorApiKey = orderedMapApiKey.entrySet().iterator();
+        final String apiKeyForWrite = entryIteratorApiKey.next().getKey();
+
+        final Iterator<Map.Entry<String, Integer>> entryIteratorAPIService = orderedMapAPIService.entrySet().iterator();
+        Map.Entry<String, Integer> entryAPIService1 = entryIteratorAPIService.next();
+        Map.Entry<String, Integer> entryAPIService2 = entryIteratorAPIService.next();
+        Map.Entry<String, Integer> entryAPIService3 = entryIteratorAPIService.next();
+        final String APIServiceForWrite1 = entryAPIService1.getKey() + " : " + entryAPIService1.getValue();
+        final String APIServiceForWrite2 = entryAPIService2.getKey() + " : " + entryAPIService2.getValue();
+        final String APIServiceForWrite3 = entryAPIService3.getKey() + " : " + entryAPIService3.getValue();
+        System.out.println(API_SERVICE);
+
+
+        final Iterator<Map.Entry<String, Integer>> entryIteratorPeakTime = orderedMapPeakTime.entrySet().iterator();
+        final String peakTimeForWrite = entryIteratorPeakTime.next().getKey();
+
+
+        final Iterator<Map.Entry<String, Integer>> entryIteratorWebBrowser = orderedMapWebBrowser.entrySet().iterator();
+        final String webBrowserForWrite = entryIteratorWebBrowser.next().getKey();
+
+
+
 
 
         try {
@@ -71,16 +106,16 @@ public class App {
 
             myWriter.write("로그 수: " + logCount + "\n\n");
 
-            myWriter.write("최다호출 APIKEY"+ "\n\n" + API_KEY + "\n\n");
+            myWriter.write("최다호출 APIKEY"+ "\n\n" + apiKeyForWrite + "\n\n");
             myWriter.write("상태코드 별 횟수\n\n" +
                     "10 : " + serverCodeCount10 + "\n" +
                     "200 : " + serverCodeCount200 + "\n" +
                     "404 : " + serverCodeCount404 + "\n\n");
-            myWriter.write("상위 3개의 API ServiceID와 각각의 요청 수\n\n" + API_SERVICE + "\n" +
-                    "blog : " + "\n" +
-                    "vdip : " + "\n" +
-                    "image : " + "\n\n");
-            myWriter.write("피크 시간대\n\n" + entryIterator.next().getKey() + "\n\n");
+            myWriter.write("상위 3개의 API ServiceID와 각각의 요청 수\n\n" +
+                    APIServiceForWrite1 + "\n" +
+                    APIServiceForWrite2 + "\n" +
+                    APIServiceForWrite3 + "\n\n");
+            myWriter.write("피크 시간대\n\n" + peakTimeForWrite + "\n\n");
             myWriter.write("웹 브라우저 별 사용비율\n\n" + WEB_BROWSER + "\n" +
                     "IE : " + "\n" +
                     "Firefox : " + "\n" +
