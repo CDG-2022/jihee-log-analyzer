3eb9f52c662b05199b8b60e662dd5dcee0d36654
GHeeJeon <jhjeon_99@naver.com>
2022-08-18 23:05:33 +0900

    클래스 별로 다시 나누는 중...

--- maven_test/src/main/java/parser/Utils.java
+++ maven_test/src/main/java/parser/Utils.java
@@ -1,4 +1,9 @@
 package parser;
 
+import java.util.Map;
+
 public class Utils {
+    public static String getPercentage (Map.Entry<String, Integer> entryWebBrowser, int logCount) {
+        return ((double)Math.round(((double)(entryWebBrowser.getValue())/(double)((logCount))*100)*100)/100)+"%";
+    }
 }
